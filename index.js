90f5b9a2d3634cc27e67788f8096c55cfb443a0b
xongjaemin <woals99@gmail.com>
2022-05-27 16:42:04 +0900

    dev: 게시물 내부에 링크 파싱

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const parser = new DomParser();
 
 const firstUrl = "https://www.ppomppu.co.kr/zboard/zboard.php?id=coupon"
 
+let linkList = [];
+let linkJson = [];
+
 const getHtml = async () => {
     try {
         return await axios({
@@ -33,20 +36,49 @@ const getContent = async (link) => {
     }
 };
 
+const hrefParsing = async(title, href) => {
+
+    let hrefBody = await getContent(href);
+    let $ = cheerio.load(iconv.decode(hrefBody.data, 'EUC-KR'));
+
+    let boardContent = $(".board-contents a").text().toString();
+
+    boardContent = boardContent.substring(boardContent.indexOf('http'));
+    
+
+    while(boardContent.indexOf('http') != -1){
+        temp = boardContent.substring(5); //'https' 빼줌
+
+        if(temp.indexOf('http') != -1){
+            linkList.push('https' + temp.substring(0, temp.indexOf('http')));
+        }
+        else{
+            linkList.push(boardContent);
+        }
+        temp = temp.substring(temp.indexOf('http'));
+
+        boardContent = temp;
+    }
+
+    linkJson.push({title: title, links: linkList});
+
+    console.log(linkJson);
+}
+
 const parsing = async () => {
     let html = await getHtml();
 
     let $ = cheerio.load(iconv.decode(html.data, 'EUC-KR'));
     const $articleList = $(".list0");
     const $articleList2 = $(".list1");
-
+    
     let articles = [];
     let hrefs = [];
-    let finalhrefs = [];
 
     $articleList.each((idx, node) => {
         const title = $(node).find('.list_title').text().toString();
         if (title.includes('네이버페이')) {
+            linkList = [];
             articles.push(title);
 
             let href = $(node).find('.list_vspace a').toString();
@@ -57,16 +89,7 @@ const parsing = async () => {
 
             href = "https://" + href;
 
-            //go to second link
-            request(href, function (err, res, body) {
-                $ = cheerio.load(body);
-                const bodyList = $("table tr").map(function (i, element) {
-                    finalhref = String($(element).find('.board-contents'));
-
-                    finalhrefs.push(finalhref);
-                });
-            });
-
+            hrefParsing(title, href);
         }
     })
     $articleList2.each((idx, node) => {
@@ -82,24 +105,13 @@ const parsing = async () => {
 
             href = "https://" + href;
 
-            //go to second link
-            request(href, function (err, res, body) {
-                $ = cheerio.load(body);
-                const bodyList = $("table tr").map(function (i, element) {
-                    finalhref = String($(element).find('.board-contents'));
-
-                    finalhrefs.push(finalhref);
-                });
-            });
-            //confirmed going into the second link
-            //해당 페이지 내의 크롤링에 문제
+            hrefParsing(title, href);
 
         }
     })
 
     console.log(articles);
     console.log(hrefs);
-    console.log(finalhrefs);
 }
 
 parsing();
