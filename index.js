920a615e783fc07a338d54150eb23dc31edb3af8
xongjaemin <woals99@gmail.com>
2022-05-31 15:01:05 +0900

    dev: 펨코 크롤링 추가

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const iconv = require("iconv-lite");
-//add module 'request'
 const DomParser = require('dom-parser');
 
 const express = require('express');
 const router = express.Router();
 
-const firstUrl = "https://www.ppomppu.co.kr/zboard/zboard.php?id=coupon"
+const firstUrl = "https://www.ppomppu.co.kr/zboard/zboard.php?id=coupon" //뽐뿌 게시판
+const secondUrl = "https://www.fmkorea.com/freedeal" //펨코 게시판
 
 let linkJson = [];
 
-const getHtml = async () => {
+const getHtml = async (targetUrl) => {
     try {
         return await axios({
-            url: firstUrl,
+            url: targetUrl,
             method: "GET",
             responseType: "arraybuffer"
         });
@@ -22,6 +22,14 @@ const getHtml = async () => {
         console.error(error);
     }
 };
+
+const getHtml2 = async (targetUrl) => {
+    try{
+        return await axios.get(targetUrl);
+    } catch(error){
+        console.log(error);
+    }
+}
 
 const getContent = async (link) => {
     try {
@@ -34,6 +42,14 @@ const getContent = async (link) => {
         console.error(error);
     }
 };
+
+const getContent2 = async (link) => {
+    try{
+        return await axios.get(link);
+    }catch(error){
+        console.log(error);
+    }
+}
 
 const hrefParsing = async(title, href) => {
     let linkList = [];
@@ -64,12 +80,66 @@ const hrefParsing = async(title, href) => {
     //console.log(linkJson);
 }
 
+const hrefParsing2 = async(title, href) => {
+    let linkList = [];
+    let hrefBody = await getContent2(href);
+
+    let $ = cheerio.load(hrefBody.data);
+
+    let boardContent = $("article a").text().toString();
+
+    //console.log(boardContent);
+
+    while(boardContent.indexOf('http') != -1){
+        temp = boardContent.substring(5);
+
+        if(temp.indexOf('http') != -1){
+            linkList.push('https' + temp.substring(0, temp.indexOf('http')));
+        }
+        else{
+            linkList.push(boardContent);
+        }
+        temp = temp.substring(temp.indexOf('http'));
+        boardContent = temp;
+    }
+
+    linkJson.push({title: title, links: linkList});
+}
+
+const init2 = async() =>{
+    let html = await getHtml2(secondUrl);
+
+    let $ = cheerio.load(html.data);
+    const $articleList3 = $(".hotdeal_var8");
+
+
+    $articleList3.each((idx, node)=>{
+        const title = $(node).find('a').text().toString();
+        if (title.includes('네이버페이')){
+
+            title_substr = title.substring(4);
+            title_substr = title_substr.slice(0,-4);
+
+            let href = $(node).find('.hotdeal_var8 a').toString();
+            href = href.substring(href.indexOf('href')+6, href.indexOf('">'));
+
+            href = "https://www.fmkorea.com" + href
+
+            hrefParsing2(title_substr, href);
+        }
+    })
+}
+
 const init = async () => {
-    let html = await getHtml();
+    //뽐뿌 게시판 크롤링
+    let html = await getHtml(firstUrl);
 
     let $ = cheerio.load(iconv.decode(html.data, 'EUC-KR'));
+    //console.log($);
     const $articleList = $(".list0");
     const $articleList2 = $(".list1");
+
+    //console.log($articleList);
     
     let articles = [];
     let hrefs = [];
@@ -82,8 +152,9 @@ const init = async () => {
             let href = $(node).find('.list_vspace a').toString();
             href = href.substring(href.indexOf('</a>') + 4,);
             href = "www.ppomppu.co.kr/zboard/" + href.substring(9, href.indexOf('>') - 1).replaceAll("&amp;", "&");
-
             hrefs.push(href);
+            
+            //console.log(href);
 
             href = "https://" + href;
 
@@ -107,6 +178,8 @@ const init = async () => {
 
         }
     })
+
+    init2();
 }
 
 init();
